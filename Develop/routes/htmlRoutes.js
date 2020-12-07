35a6e88b5287fe13d6143a0df1ceb6e23ee80d1a
arurian <arunvb2004@gmail.com>
2020-12-07 20:28:24 +1030

    initial commit

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+const path = require("path");
+//const router = require("express").Router();
+// exporting html paths
+module.exports = function(app) {
+    
+    app.get("/", function(req, res) {
+        res.json(path.join(__dirname, "../public/index.html"));
+      });
+    
+    app.get("/notes", function(req,res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
+   // set index.html as default page
+   
+   app.get("*", function(req,res) {
+       res.sendFile(path.join(__dirname, "../public/index.html"));
+     });
+
+};
