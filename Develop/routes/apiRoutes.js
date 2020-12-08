e25b1f0f845fc4d381b364a1c9b568362b4c7617
arurian <arunvb2004@gmail.com>
2020-12-08 18:40:23 +1030

    changed

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+//var data = JSON.parse(fs.readFileSync("../db/db.json", "utf8"));
 
 
 module.exports = function(app) {
